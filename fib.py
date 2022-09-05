31acbd1bc60a57de0fe7c837d6341f2819c78cdf
MonkeyJerr <ajm3@andrew.cmu.edu>
2022-09-04 20:09:10 -0400

    small change to edge case

--- fib.py
+++ fib.py
@@ -8,7 +8,7 @@ Negative numbers should return None
 def fibonacci(position):
   if(position <= 1):
     return position
-  if(position > 0):
+  if(position < 0):
     return None
   return fibonacci(position - 1) + fibonacci(position - 2)
 
